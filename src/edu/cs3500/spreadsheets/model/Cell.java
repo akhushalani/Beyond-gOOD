b02ede09e2f457d7a2c3d8951dba8f9766944958
abhinavkhushalani <ask9908@me.com>
2019-10-23 18:07:33 -0400

    Added Cell interface

---
+++ src/edu/cs3500/spreadsheets/model/Cell.java
@@ -0,0 +1,15 @@
+package edu.cs3500.spreadsheets.model;
+
+import java.util.ArrayList;
+
+/**
+ * A broad interface for handling cells in a spreadsheet of any type.
+ */
+public interface Cell {
+  /**
+   * Check which cells the current Cell is directly or indirectly referenced by.
+   *
+   * @return ArrayList of Coords that reference this cell.
+   */
+  ArrayList referencedBy();
+}
