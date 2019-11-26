f161c9e62923741d25ba0816aa028a5ff5b99630
abhinavkhushalani <ask9908@me.com>
2019-11-25 20:27:28 -0500

    Added javadoc for the view interface

--- src/edu/cs3500/spreadsheets/view/WorksheetView.java
+++ src/edu/cs3500/spreadsheets/view/WorksheetView.java
@@ -22,21 +22,59 @@ public interface WorksheetView {
    */
   Appendable getAppendable();
 
+  /**
+   * Checks if any cells are selected in the worksheet.
+   * @return if any cells are selected
+   */
   boolean cellsSelected();
 
+  /**
+   * Gets the first selected cell in the selection.
+   * @return the Coord of the first selected cell
+   */
   Coord getFirstSelected();
 
+  /**
+   * Gets the top left cell in the selection.
+   * @return the Coord of the top left selected cell
+   */
   Coord getMinSelection();
 
+  /**
+   * Gets the bottom right cell in the selection.
+   * @return the Coord of the bottom right cell
+   */
   Coord getMaxSelection();
 
+  /**
+   * Gets the text that has been typed in the cell edit field.
+   * @return the String in the edit field
+   */
   String getEditText();
 
+  /**
+   * Sets the text visible in the cell edit field.
+   * @param editText the String to put in the edit field
+   */
   void setEditText(String editText);
 
+  /**
+   * Notifies the view that a cell's data has been changed.
+   * @param coord the location of the changed cell
+   */
   void notifyCellChanged(Coord coord);
 
+  /**
+   * Adds the feature listeners to the view.
+   * @param clicks the listener for button actions
+   * @param cellEdits the listener for changes to the edit field text
+   * @param keys the keyboard listener
+   */
   void setListeners(ActionListener clicks, DocumentListener cellEdits, KeyListener keys);
 
+  /**
+   * Sets the title of the window containing the view.
+   * @param title the title to set
+   */
   void setWindowTitle(String title);
 }
